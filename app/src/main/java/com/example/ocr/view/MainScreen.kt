739e69ca7620fc676133a26af3f24b48ea6771f6
Ashwani1330 <ashwanihabri@gmail.com>
2024-09-04 15:05:49 +0530

    Photo, Gallery ✅

--- app/src/main/java/com/example/ocr/view/MainScreen.kt
+++ app/src/main/java/com/example/ocr/view/MainScreen.kt
@@ -1,22 +1,48 @@
 package com.example.ocr.view
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ocr.MainViewModel
+import kotlinx.coroutines.launch
 import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(context: Context) {
+    val scope = rememberCoroutineScope()
+
     val scaffoldState = rememberBottomSheetScaffoldState()
 
     val controller = remember {
@@ -27,15 +53,21 @@ fun MainScreen(context: Context) {
         }
     }
 
+    val viewModel = viewModel<MainViewModel>()
+    val bitmaps by viewModel.bitmaps.collectAsState()
 
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         sheetPeekHeight = 0.dp,
         sheetContent = {
-
+            PhotoBottomSheetContent(
+                bitmaps = bitmaps,
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxWidth()
+            )
         }
     ) {
-        padding ->
+            padding ->
         Box(
             modifier = androidx.compose.ui.Modifier
                 .fillMaxSize()
@@ -46,6 +78,85 @@ fun MainScreen(context: Context) {
                 modifier = androidx.compose.ui.Modifier
                     .fillMaxSize()
             )
+
+            IconButton(onClick = {
+                controller.cameraSelector = if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                    CameraSelector.DEFAULT_FRONT_CAMERA
+                } else {
+                    CameraSelector.DEFAULT_BACK_CAMERA
+                }
+            },
+                modifier = androidx.compose.ui.Modifier
+                    .offset(16.dp, 16.dp)
+
+            ) {
+                Icon(imageVector = Icons.Default.Refresh, contentDescription = "Switch camera" )
+            }
+            Row(
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceAround
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
+
+                    },
+                ) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Open Gallery")
+
+                }
+
+                IconButton(
+                    onClick = {
+                        takePhoto(
+                            controller = controller,
+                            onPhotoTaken = viewModel::onTakePhoto,
+                            context
+                        )
+                    },
+                ) {
+                    Icon(imageVector = Icons.Default.Face, contentDescription = "Take Photo")
+
+                }
+            }
         }
     }
+}
+
+private fun takePhoto(controller: LifecycleCameraController,
+                      onPhotoTaken: (Bitmap) -> Unit, context: Context) {
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object: OnImageCapturedCallback(){
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                    // postScale(-1f, 1f)
+                }
+                val rotatedBitmap = Bitmap.createBitmap(
+                    image.toBitmap(),
+                    0,
+                    0,
+                    image.width,
+                    image.height,
+                    matrix,
+                    true
+                )
+
+                onPhotoTaken(rotatedBitmap)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+                Log.e("Camera", "Couldn't take photo: ", exception)
+            }
+        }
+    )
 }
